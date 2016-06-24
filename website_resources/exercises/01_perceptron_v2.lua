d39e981252350177c95a8d80f17066cb5422a04f
Matthias Dumke <matthias.dumke@gmx.net>
2016-06-24 15:00:59 +0900

    finish perceptron exercise

--- website_resources/exercises/01_perceptron_v2.lua
+++ website_resources/exercises/01_perceptron_v2.lua
@@ -5,6 +5,7 @@
   artificial data
 --]]
 
+require('gnuplot')
 math.randomseed(os.time())
 
 -- returns an nx3 tensor, dim1 is filled with 1s, dim2,3 with random values
@@ -14,15 +15,36 @@ initialize_training_input = function (n)
   return xs
 end
 
--- returns the sum of all values in the given table
-sum_array = function (arr)
-  sum = 0
+-- plots the given functions
+plot = function (f, g)
+  f_x = torch.linspace(-1, 1)
+  f_y = torch.linspace(-1, 1):apply(f)
 
-  for _, val in pairs(arr) do
-    sum = sum + val
-  end
+  g_x = torch.linspace(-1, 1)
+  g_y = torch.linspace(-1, 1):apply(g)
 
-  return sum
+  gnuplot.figure(1)
+  gnuplot.axis({-1, 1, -1, 1})
+  gnuplot.xlabel('x')
+  gnuplot.ylabel('y')
+  gnuplot.title('target vs hypothesis')
+
+  gnuplot.plot(
+    {'f(x)', f_x, f_y, '-'},
+    {'g(x)', g_x, g_y, '-'}
+  )
+end
+
+-- prints the given numbers to the command line
+report_results = function (num_trials, training_examples, avg_steps, avg_p_miss)
+  print('--------')
+  print('- Perceptron Learning Algorithm')
+  print('-')
+  print('- running with ' .. num_trials .. ' trials, each with ' ..
+    training_examples .. ' training examples')
+  print('-  * average convergence after ' .. avg_steps .. ' steps')
+  print('-  * average missclassification rate is ' .. avg_p_miss)
+  print('--------')
 end
 
 -- returns a radom value from [-1, 1]
@@ -59,8 +81,8 @@ generate_target_function = function ()
 end
 
 -- returns +1 if the function returns a positive value, else -1
-evaluate_point = function (point, f)
-  if f(point[2]) > point[3] then
+evaluate_point = function (x1, x2, f)
+  if f(x1) > x2 then
     return 1
   else
     return -1
@@ -72,7 +94,7 @@ evaluate_target_function = function (data, f)
   local y = torch.Tensor(data:size()[1])
 
   for i, slice in ipairs(data:split(1, 1)) do
-    y[i] = evaluate_point(slice[1], f)
+    y[i] = evaluate_point(slice[1][2], slice[1][3], f)
   end
 
   return y
@@ -86,16 +108,31 @@ end
 -- returns a linear function extracted from the given weights vector
 compute_hypothesis = function(w)
   local a = -(w[2][1] / w[3][1])
-  local b = w[1][1] / w[3][1]
+  local b = -(w[1][1]) / w[3][1]
 
   return function (x)
     return a * x + b
   end
 end
 
+-- returns the probability that f and g disagree on a point, uses n examples
+validate = function(f, g, n)
+  local total_miss = 0
+
+  for i = 1, n do
+    local x = {rand_val(), rand_val()}
+
+    if evaluate_point(x[1], x[2], f) ~= evaluate_point(x[1], x[2], g) then
+      total_miss = total_miss + 1
+    end
+  end
+
+  return total_miss / n
+end
+
 -- returns a linear function that explains the point-classes
-learn_approximation = function (xs, y)
-  w = torch.Tensor(3, 1):fill(0)
+learn_approximation = function (xs, y, f)
+  local w = torch.Tensor(3, 1):fill(0)
   local n = xs:size()[1]
   local success = false
   local steps = 0
@@ -114,35 +151,56 @@ learn_approximation = function (xs, y)
     end
   end
 
-  return compute_hypothesis(w), steps
+  local g = compute_hypothesis(w)
+  local validation_set_size = 1000
+  local p_miss = validate(f, g, validation_set_size)
+
+  return g, p_miss, steps
 end
 
--- returns a hypothesis and #steps for a new target function
+-- returns #steps to comp. g and P(g is wrong), for random target fn
 -- n: integer, number of generated training examples
 run_perceptron = function (n)
-  xs = initialize_training_input(n)
-  f = generate_target_function()
-  y = evaluate_target_function(xs, f)
-  g, steps = learn_approximation(xs, y)
+  local xs = initialize_training_input(n)
+  local f = generate_target_function()
+  local y = evaluate_target_function(xs, f)
 
-  return g, steps
+  g, p_miss, steps = learn_approximation(xs, y, f)
+
+  return f, g, p_miss, steps
+end
+
+-- returns average number of missclass. and steps after simulation
+run_simulation = function (num_trials, num_training_examples)
+  local total_steps = 0
+  local total_p_miss = 0
+
+  for i = 1, num_trials do
+    f, g, p_miss, steps = run_perceptron(num_training_examples)
+    total_steps = total_steps + steps
+    total_p_miss = total_p_miss + p_miss
+  end
+
+  -- plot(f, g)
+
+  local avg_steps = total_steps / num_trials
+  local avg_p_miss = total_p_miss / num_trials
+
+  return avg_steps, avg_p_miss
 end
 
 
 
+---------------- MAIN CONTROL ------------------
 
-training_examples = 10
+training_examples = 100
 num_trials = 1000
-all_steps = {}
 
-for i = 1, num_trials do
-  g, steps = run_perceptron(training_examples)
-  all_steps[#all_steps + 1] = steps
-end
-
-print(sum_array(all_steps) / num_trials)
-
-
+report_results(
+  num_trials,
+  training_examples,
+  run_simulation(num_trials, training_examples)
+)
 
 
 
