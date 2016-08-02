69c9e7681688c862dfc5c1ff27418c2d4ced8c30
Matthias Dumke <matthias.dumke@gmx.net>
2016-08-02 12:29:06 +0200

    add code to compute the variance-estiate

--- website_resources/exercises/04_bias_variance.lua
+++ website_resources/exercises/04_bias_variance.lua
@@ -29,7 +29,7 @@ plot_estimate = function (g_bar)
 
   gnuplot.plot(
     {'target', sin_x, sin_y, '-'},
-    {'g_bar', hyp_x, hyp_y, '-'})
+    {'g bar', hyp_x, hyp_y, '-'})
 end
 
 -- returns the target function sin(pi * x)
@@ -40,8 +40,8 @@ get_target_function = function ()
 end
 
 -- returns 2 training points
-get_training_data = function ()
-  local x = torch.rand(1, 2) * 2 - 1
+get_training_data = function (n)
+  local x = torch.rand(1, n) * 2 - 1
   local y = x:clone():apply(get_target_function())
   return x, y
 end
@@ -54,8 +54,8 @@ compute_hypothesis = function (w)
 end
 
 -- returns the weight from a single trial
-run_once = function ()
-  local x, y = get_training_data()
+single_hypothesis = function ()
+  local x, y = get_training_data(2)
   local pseudo_inverse = x:t() * torch.inverse(x * x:t())
   return y * pseudo_inverse
 end
@@ -65,13 +65,40 @@ run_simulation = function (num_trials)
   local hypotheses = torch.Tensor(num_trials)
 
   for i = 1, num_trials do
-    hypotheses[i] = run_once()
+    hypotheses[i] = single_hypothesis()
   end
 
   return round(torch.mean(hypotheses))
 end
 
-g_bar = run_simulation(2000)
+-- generates multiple random points and return the estimated bias
+compute_bias_estimate = function (g_bar)
+  test_x, test_y = get_training_data(1000)
+  predictions = test_x:clone() * g_bar
+  return torch.mean(torch.pow(predictions - test_y, 2))
+end
+
+-- generates random samples to estimate the variance
+compute_variance_estimate = function (g_bar)
+  local n = 1000
+  errors = torch.Tensor(n)
+
+  for i = 1, n do
+    local g = single_hypothesis()[1][1]
+    local x = torch.rand(1, 1000) * 2 - 1
+
+    local y_g = x:clone() * g
+    local y_g_bar = x:clone() * g_bar
+
+    errors[i] = torch.mean(torch.pow(y_g - y_g_bar, 2))
+  end
+
+  return torch.mean(errors)
+end
+
+g_bar = run_simulation(1000)
 plot_estimate(g_bar)
 print("Best hypothesis estimate: " .. g_bar)
+print("bias estimate: " .. round(compute_bias_estimate(g_bar)))
+print("variance estimate: " .. round(compute_variance_estimate(g_bar)))
 
