a81bdac3e259fd4cd516c0f8aa4311762e189edd
Matthias Dumke <matthias.dumke@gmx.net>
2016-09-16 08:17:13 +0200

    begin regularization-problem

---
+++ website_resources/exercises/06_regularization.lua
@@ -0,0 +1,12 @@
+#!/home/tias/torch/install/bin/th
+
+--[[
+  Regularization with Weight Decay
+
+  Start by performing a simple linear regression on the data provided.
+--]]
+
+require('helpers')
+
+x, y = read_data_from_file('in.dta')
+
---
+++ website_resources/exercises/helpers.lua
@@ -0,0 +1,36 @@
+-- returns the number of lines in the file
+num_lines = function (filename)
+  local handle = io.popen("wc -l < " .. filename)
+  local result = handle:read("*a")
+  handle:close()
+  return tonumber(result)
+end
+
+-- reads in data from the given file
+read_data_from_file = function (filename)
+  local length = num_lines(filename)
+  local x = torch.Tensor(length, 2)
+  local y = torch.Tensor(length, 1)
+
+  local line_number = 1
+
+  for line in io.lines(filename) do
+    local entry = 1
+
+    for number in line:gmatch("%S+") do
+      if entry  == 1 then
+        x[line_number][1] = tonumber(number)
+      elseif entry == 2 then
+        x[line_number][2] = tonumber(number)
+      else
+        y[line_number][1] = tonumber(number)
+      end
+
+      entry = entry + 1;
+    end
+
+    line_number = line_number + 1
+  end
+
+  return x, y
+end
